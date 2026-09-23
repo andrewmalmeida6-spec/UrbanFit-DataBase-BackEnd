fa4b6d360045ecba08beedb442fa686693c32982
andrewmalmeida6-spec <andrew.m.almeida6@aluno.senai.br>
2026-09-23 09:59:34 -0300

    removendo db seed

--- src/routes/funcionario.routes.ts
+++ src/routes/funcionario.routes.ts
@@ -1,12 +1,12 @@
 import * as funcionarioController from '../controllers/funcionario.controller';
 import { Router } from 'express';
-import { autorizarCargo } from '../middlewares/auth.middleware';
+import { autorizar, autorizarCargo } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 router.use(autorizarCargo('administrador')),
 
-router.post('/', funcionarioController.criar);
+router.post('/', autorizar('cliente'), funcionarioController.criar);
 router.get('/', funcionarioController.listar);
 router.get('/:id', funcionarioController.buscarPorID);
 
